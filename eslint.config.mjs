9830bc25a7431f57678e60f71160c85b2dbba433
Muhammad Asad <asadmahmood41999@gmail.com>
2025-05-04 08:32:52 +0500

    Setup Tailiwnd, husky, eslint, prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,42 @@
+import pluginJs from '@eslint/js'
+import configPrettier from 'eslint-config-prettier'
+import pluginImport from 'eslint-plugin-import'
+import pluginPrettier from 'eslint-plugin-prettier'
+import pluginReact from 'eslint-plugin-react'
+import globals from 'globals'
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    languageOptions: {
+      sourceType: 'module',
+      globals: globals.browser,
+    },
+    plugins: {
+      import: pluginImport,
+      prettier: pluginPrettier,
+    },
+    rules: {
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+          ],
+          alphabetize: { order: 'asc', caseInsensitive: true },
+          'newlines-between': 'always',
+        },
+      ],
+      'prettier/prettier': 'error',
+    },
+  },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  configPrettier,
+]
